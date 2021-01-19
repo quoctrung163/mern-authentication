7369287877296853f9250c5d33c1e9d03f44dc4f
quoctrung163 <miyucoder@gmail.com>
2021-01-19 15:35:09 +0700

    done signup page

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './core/Home';
+import Users from './user/Users';
+import Signup from './user/Signup';
+import Menu from './core/Menu';
 
 const MainRouter = () => {
   return (
     <div>
+      <Menu />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/users' component={Users} />
+        <Route path='/signup' component={Signup} />
       </Switch>
     </div>
   )
--- client/user/api-user.js
+++ client/user/api-user.js
@@ -59,3 +59,21 @@ const update = async (params, credentials, user) => {
     console.log(err);
   }
 }
+
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch('/api/users/' + params.userId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { create, list, read, update, remove };
