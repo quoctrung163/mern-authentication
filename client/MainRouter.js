f5998609d44b8fa8e9e44127396528bed1499d3e
quoctrung163 <miyucoder@gmail.com>
2021-01-20 09:22:33 +0700

    validation props and delete user

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,11 +1,14 @@
-import { divide } from 'lodash';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './core/Home';
 import Users from './user/Users';
 import Signup from './user/Signup';
+import Signin from './auth/Signin';
+import Profile from './user/Profile';
 import Menu from './core/Menu';
+import PrivateRoute from './auth/PrivateRoute';
+import EditProfile from './user/EditProfile';
 
 const MainRouter = () => {
   return (
@@ -15,6 +18,9 @@ const MainRouter = () => {
         <Route exact path='/' component={Home} />
         <Route path='/users' component={Users} />
         <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
+        <PrivateRoute path='/user/edit/:userId' component={EditProfile} />
+        <Route path="/user/:userId" component={Profile} />
       </Switch>
     </div>
   )
--- client/user/Signup.js
+++ client/user/Signup.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import { makeStyles } from '@material-ui/core/styles';
-import { create } from './api-user.js';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Icon from '@material-ui/core/Icon'
+import { makeStyles } from '@material-ui/core/styles'
+import { create } from './api-user.js'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -42,15 +41,15 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const Signup = () => {
-  const classes = useStyles();
+export default function Signup() {
+  const classes = useStyles()
   const [values, setValues] = useState({
     name: '',
     password: '',
     email: '',
     open: false,
     error: ''
-  });
+  })
 
   const handleChange = name => event => {
     setValues({ ...values, [name]: event.target.value })
@@ -62,72 +61,49 @@ const Signup = () => {
       email: values.email || undefined,
       password: values.password || undefined
     }
-
-    create(user).then(data => {
+    create(user).then((data) => {
       if (data.error) {
         setValues({ ...values, error: data.error })
       } else {
         setValues({ ...values, error: '', open: true })
       }
     })
-
   }
 
-  return (
-    <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Sign up
+  return (<div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6" className={classes.title}>
+          Sign Up
           </Typography>
-          <TextField
-            id="name" label="Name"
-            className={classes.textField}
-            value={values.name} onChange={handleChange('name')}
-            margin="normal"
-          />
-          <br />
-          <TextField id="email" type="email" label="Email"
-            className={classes.textField}
-            value={values.email} onChange={handleChange('email')}
-            margin="normal" />
-          <br />
-          <TextField id="password" type="password" label="Password"
-            className={classes.textField} value={values.password}
-            onChange={handleChange('password')} margin="normal" />
-          <br />
-          {
-            value.error && (
-              <Typography component="p" color="error">
-                <Icon color="error" className={classes.error}>error</Icon>
-                {values.error}
-              </Typography>
-            )
-          }
-        </CardContent>
-        <CardActions>
-          <Button color="primary" variant="contained"
-            onClick={clickSubmit}
-            className={classes.submit}>Submit</Button>
-        </CardActions>
-      </Card>
-      <Dialog open={values.open} disableBackdropClick={true}>
-        <DialogTitle>New Account</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            New account successfully created.
+        <TextField id="name" label="Name" className={classes.textField} value={values.name} onChange={handleChange('name')} margin="normal" /><br />
+        <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal" /><br />
+        <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal" />
+        <br /> {
+          values.error && (<Typography component="p" color="error">
+            <Icon color="error" className={classes.error}>error</Icon>
+            {values.error}</Typography>)
+        }
+      </CardContent>
+      <CardActions>
+        <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
+      </CardActions>
+    </Card>
+    <Dialog open={values.open} disableBackdropClick={true}>
+      <DialogTitle>New Account</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          New account successfully created.
           </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Link to="/signin">
-            <Button color="primary" autoFocus="autoFocus" variant="contained">
-              Sign In
+      </DialogContent>
+      <DialogActions>
+        <Link to="/signin">
+          <Button color="primary" autoFocus="autoFocus" variant="contained">
+            Sign In
             </Button>
-          </Link>
-        </DialogActions>
-      </Dialog>
-    </div>
+        </Link>
+      </DialogActions>
+    </Dialog>
+  </div>
   )
-};
-
-export default Signup;
+}
